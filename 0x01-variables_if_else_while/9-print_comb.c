846fff1cfc318765fd1858a797ae5575deac007f
Edgar Santander <3823@holbertonschool.com>
2021-10-01 14:13:47 -0700

    0x01 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int a;
+
+/* while loop execution */
+for (a = 48; a < 58; a++)
+{
+putchar(a);
+
+if (a < 57)
+{
+putchar(',');
+putchar(' ');
+}
+}
+
+putchar('\n');
+
+return (0);
+}
