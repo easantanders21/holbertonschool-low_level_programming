e0d89cc8a5156019d73aadebc871e6299b40218d
Edgar Santander <3823@holbertonschool.com>
2021-10-01 13:20:56 -0700

    0x01 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int a;
+
+/* while loop execution */
+for (a = 48; a < 58; a++)
+{
+putchar(a);
+}
+
+putchar('\n');
+
+return (0);
+}
