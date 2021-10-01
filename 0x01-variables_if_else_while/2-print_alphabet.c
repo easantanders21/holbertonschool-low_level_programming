fb19ba6d37cfe7fda5cec3d0594d8ef60df26598
Edgar Santander <3823@holbertonschool.com>
2021-10-01 12:22:48 -0700

    0x01 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+char l = 'a';
+
+do {
+
+printf("%c", l);
+l++;
+
+} while (l <= 'z');
+
+printf("\n");
+
+return (0);
+}
