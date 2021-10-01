432e4e0fce95c3b535392a95e1635e81eec2767d
Edgar Santander <3823@holbertonschool.com>
2021-10-01 13:47:47 -0700

    0x01 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+int a;
+int b;
+/* while loop execution */
+for (a = 48; a < 58; a++)
+{
+putchar(a);
+}
+
+for (b = 97; b < 103; b++)
+{
+putchar(b);
+}
+
+putchar('\n');
+
+return (0);
+}
