3eeb22ab270d9b26b0e13a6a9f906091e6c831f2
Edgar Santander <3823@holbertonschool.com>
2021-10-01 13:04:09 -0700

    0x01 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+/* local variable definition */
+int a;
+/* while loop execution */
+for (a = 0; a < 10; a++)
+{
+printf("%d", a);
+}
+
+printf("\n");
+
+return (0);
+}
