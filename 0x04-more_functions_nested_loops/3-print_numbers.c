f0af205af992fa958008dfcf430c6669d702c7a0
easantanders21 <3823@holbertonschool.com>
2021-10-08 18:15:45 +0000

    3 print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_numbers - print 0 - 9
+ *
+ *
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10 ; i++)
+	{
+		_putchar(i + 48);
+	}
+	_putchar('\n');
+}
