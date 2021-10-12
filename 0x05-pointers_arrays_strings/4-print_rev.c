dc950cd51b75b338e785f476d87d9a48ca18b763
easantanders21 <3823@holbertonschool.com>
2021-10-12 00:23:25 +0000

    pointers 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_rev - print reverse
+ * @s: str
+ *
+ *
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int j = 0;
+	int i = 0;
+	int k = 0;
+
+	while (*(s + j))
+	{
+		j++;
+	}
+
+	i = j;
+
+	for (k = i; k >= 0; k--)
+	{
+		_putchar (s[k]);
+	}
+
+	_putchar ('\n');
+}
