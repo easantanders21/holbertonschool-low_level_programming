47f87a4429aca2052d9c6cb093aaaeda734e8f05
easantanders21 <3823@holbertonschool.com>
2021-10-12 02:49:42 +0000

    pointers

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * rev_string - print reverse
+ * @s: str
+ *
+ *
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	char cadena[10];
+	int j = 0;
+	int i = 0;
+	int k = 0;
+
+	while (*(s + j))
+	{
+		j++;
+	}
+
+	i = j;
+
+	for (k = 0; k <= i; k++)
+	{
+		cadena[k] = s[k];
+	}
+
+	for (k = 0; k < i; k++)
+	{
+		*(s + k) = cadena[i - 1 - k];
+
+	}
+}
