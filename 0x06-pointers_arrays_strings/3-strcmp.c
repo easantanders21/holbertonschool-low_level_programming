c7e0e6a7b83a8faea9699c7f67793c1351044219
easantanders21 <3823@holbertonschool.com>
2021-10-14 14:48:09 +0000

    0x06 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strcmp - s1 compare s2
+ * @s1: comp 1
+ * @s2: comp 2
+ *
+ *
+ * Return: compare
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int r = 0;
+
+	r = s1[0] - s2[0];
+
+	return (r);
+}
