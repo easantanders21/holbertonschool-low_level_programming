a9c1bccc638783991de86dd5a47eefd358ed7242
easantanders21 <3823@holbertonschool.com>
2021-10-15 01:22:49 +0000

    0x06 - 6 x3

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,10 +10,14 @@
 char *cap_string(char *s)
 {
 	int i = 0;
+	int j = 0;
 
-	i = 0;
+	while (*(s + j))
+	{
+		j++;
+	}
 
-	while (*(s + i))
+	for (i = 0; i < j; i++)
 	{
 		if (s[i] == ' ' || s[i] == '\n' || s[i] == ',' || s[i] == ';'
 		|| s[i] == '.' || s[i] == '?' || s[i] == '"' || s[i] == '('
@@ -25,8 +29,6 @@ char *cap_string(char *s)
 			s[i + 1] = s[i + 1] - 32;
 		}
 		}
-
-		i++;
 	}
 
 	return (s);
