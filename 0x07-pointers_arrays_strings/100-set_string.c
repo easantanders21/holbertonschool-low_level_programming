38d7ee47385f3879aa54b4f98ef8c6a5411cc427
easantanders21 <3823@holbertonschool.com>
2021-10-19 17:27:06 +0000

    100

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * set_string - set string
+ * @s: pointer to pointer
+ * @to: s1
+ *
+ *
+ * Return: void
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
