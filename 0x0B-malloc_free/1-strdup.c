5f068ff90ad5700f561253730aefca8fd0f39b74
easantanders21 <3823@holbertonschool.com>
2021-10-26 21:18:47 +0000

    malloc task 1 x2

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -14,12 +14,17 @@ char *_strdup(char *str)
 	int j = 0;
 	char *str2;
 
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
 	while (*(str + i))
 	{
 		i++;
 	}
 
-	str2 = malloc(sizeof(char) * (i));
+	str2 = malloc(sizeof(char) * (i + 1));
 
 	if (str2 == NULL)
 
