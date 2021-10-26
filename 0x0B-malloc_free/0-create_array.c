0c8e8fd6b46c9139757c202c6fa3abcad5253602
easantanders21 <3823@holbertonschool.com>
2021-10-26 20:42:41 +0000

    malloc task 0 x3

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -22,15 +22,15 @@ char *create_array(unsigned int size, char c)
 	{
 	str = malloc(sizeof(char) * size);
 
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
 		for (i = 0; i < size; i++)
 		{
 			str[i] = c;
 		}
-
-	return (str);
 	}
-	else
-	{
-		return (NULL);
-	}
+		return (str);
 }
