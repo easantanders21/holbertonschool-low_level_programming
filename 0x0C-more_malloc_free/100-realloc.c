f965476324eb79dd4ee66ae0e17a8a89829fd683
easantanders21 <3823@holbertonschool.com>
2021-10-29 19:06:38 +0000

    100

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _realloc - array range
+ * @ptr: pointer to char
+ * @old_size: old size
+ * @new_size: new size
+ *
+ * Return: void
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *p;
+
+	if (ptr == NULL)
+	{
+		p = malloc(new_size);
+		return (p);
+	}
+
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	if (new_size == old_size)
+	{
+		return (ptr);
+	}
+
+	p = malloc(new_size);
+	(void) old_size;
+
+	return (p);
+}
