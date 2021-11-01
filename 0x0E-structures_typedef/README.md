90366fb51abce455d2dc96c5897894fe160ff19a
easantanders21 <3823@holbertonschool.com>
2021-11-01 15:04:46 +0000

    dog

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+_Structures and typedef_
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_FILE
+#define MAIN_FILE
+/**
+ * struct dog - dog structure
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ *
+ * Description: dog structure
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
