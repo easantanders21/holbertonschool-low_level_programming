ef810bf741d6268665be8e8be485369dd9c357ad
easantanders21 <3823@holbertonschool.com>
2021-11-04 21:49:49 +0000

    task 1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,6 +13,9 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list ap;
 	unsigned int i;
 
+	if (!separator)
+		exit(-1);
+
 	va_start(ap, n);
 	for (i = 0; i < n; i++)
 	{
