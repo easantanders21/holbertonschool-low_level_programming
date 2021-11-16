35f30231a05fcd627352b9491dad0baa9e9b81a7
easantanders21 <3823@holbertonschool.com>
2021-11-16 16:07:13 +0000

    task 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -17,13 +17,13 @@ void free_listint2(listint_t **head)
 		return;
 
 	tmp2 = *head;
+	tmp = tmp2;
 
 	while (tmp2 != NULL)
 	{
-		tmp = tmp2;
 		tmp2 = tmp2->next;
 		free(tmp);
+		tmp = tmp2;
 	}
-	free(tmp2);
 	*head = NULL;
 }
