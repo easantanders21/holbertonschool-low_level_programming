e2a1e9878b4ca4d0dfd7fd31723f21e40cc8671a
easantanders21 <3823@holbertonschool.com>
2021-11-16 18:50:14 +0000

    task 10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -14,7 +14,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *tmp, *tmp1, *tmp2;
 	unsigned int i = 0;
 
-	if (*head == NULL)
+	if (*head == NULL || head == NULL)
 		return (1);
 
 	tmp = *head;
