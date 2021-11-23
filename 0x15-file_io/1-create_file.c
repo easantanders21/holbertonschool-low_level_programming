105d74b818a82fc4e707d94b3cfa366a152d7dce
easantanders21 <3823@holbertonschool.com>
2021-11-23 14:26:10 +0000

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,7 +18,7 @@ int create_file(const char *filename, char *text_content)
 
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
-	if (fd == -1)
+	if (fd < 3)
 		return (-1);
 
 	while (*(text_content + i))
