b8cfc488f977b3c3daac0eb937997a4ff72a641d
easantanders21 <3823@holbertonschool.com>
2021-11-23 14:55:46 +0000

    task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -19,7 +19,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	char *buffer;
 	ssize_t nr_bytes;
 
-	if (filename == NULL)
+	if (filename == NULL || letters == 0)
 		return (0);
 
 	buffer = (char *)malloc(letters);
