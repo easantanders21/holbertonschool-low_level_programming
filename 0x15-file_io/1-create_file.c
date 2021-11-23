33f622b3fab38c06cf515ba1720ad7334180caba
easantanders21 <3823@holbertonschool.com>
2021-11-23 15:09:08 +0000

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd, i = 0, testw;
 
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
 
 	if (fd < 3)
 		return (-1);
