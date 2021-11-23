bb59e39328f60d887d3286065ce17ed8705f628a
easantanders21 <3823@holbertonschool.com>
2021-11-23 22:02:10 +0000

    task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -40,8 +40,8 @@ int main(int ac, char **av)
 		exit(99); }
 
 	if (numRead == -1)
-	{ dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-		exit(99); }
+	{ dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+		exit(98); }
 
 	if (close(fdI) == -1)
 	{ dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fdI);
