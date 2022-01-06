b60ea75716ca25d1086d46d553b4baf61242d9a7
easantanders21 <3823@holbertonschool.com>
2022-01-06 18:22:44 +0000

    task 2

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+/**
+ * add_dnodeint - add node to list
+ * @head: head to list
+ * @n: integer value
+ * Return: list
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *temp = *head;
+	dlistint_t *new;
+
+	new = malloc(sizeof(dlistint_t));
+	if (temp == NULL)
+	{
+		new->n = 0;
+		new->next = NULL;
+		new->prev = NULL;
+		*head = new;
+		return (*head);
+	}
+	new->n = n;
+	temp->prev = new;
+	new->next = temp;
+	new->prev = NULL;
+	*head = new;
+	return (*head);
+}
