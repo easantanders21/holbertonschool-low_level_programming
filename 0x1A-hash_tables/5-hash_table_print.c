03e3b1375a1a3f9638d2d748afa8694585406f5e
easantanders21 <3823@holbertonschool.com>
2022-01-28 19:42:25 +0000

    task 5

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "hash_tables.h"
+/**
+ * hash_table_print - add nodes to table
+ * @ht: hash table
+ * @key: is a key
+ * @value: is the size
+ *
+ * Return: index value.
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *node = NULL;
+	unsigned long int i = 0;
+	int flag = 0;
+
+	if (ht == NULL)
+		return;
+
+	printf("{");
+	while (i <= ht->size)
+	{
+		node = ht->array[i];
+		if (ht->array[i] != NULL)
+		{
+
+			while (node != NULL)
+			{
+				if (flag != 0)
+					printf(", ");
+				printf("\'%s\': \'%s\'", node->key, node->value);
+				node = node->next;
+				flag = 1;
+			}
+		}
+		i++;
+	}
+	printf("}\n");
+}
