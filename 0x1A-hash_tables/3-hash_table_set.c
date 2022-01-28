280a906ec92df5f259059e7ffca8fd8284754e75
easantanders21 <3823@holbertonschool.com>
2022-01-28 19:10:38 +0000

    task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -36,7 +36,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		}
 
 	}
-
 	new = malloc(sizeof(hash_node_t));
 	if (new == NULL)
 		return (0);
@@ -51,7 +50,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		ht->array[index] = new;
 		return (1);
 	}
-
 	ht->array[index] = new;
 	return (1);
 }
