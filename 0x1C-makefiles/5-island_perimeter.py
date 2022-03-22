7d7eca2a7a54275ef50f4e38e7e5a486fbb88cbf
easantanders21 <3823@holbertonschool.com>
2022-03-21 20:07:29 -0500

    task 5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -6,10 +6,11 @@
 
 def island_perimeter(grid):
     """ Island perimeter function"""
+    perimeter = 0
+    if grid is None:
+        return perimeter
     height = len(grid)
     width = len(grid[0])
-    num_islas = 0
-    perimeter = 0
     for i in range(height):
         for j in range(width):
             if grid[i][j] == 1:
@@ -24,13 +25,13 @@ def island_perimeter(grid):
                 if i > 0:
                     if grid[i-1][j] == 0:
                         perimeter += 1
-                if i < width - 1:
+                if i < height - 1:
                     if grid[i+1][j] == 0:
                         perimeter += 1
                 if j > 0:
                     if grid[i][j-1] == 0:
                         perimeter += 1
-                if j < height - 1:
+                if j < width - 1:
                     if grid[i][j+1] == 0:
                         perimeter += 1
     return(perimeter)
